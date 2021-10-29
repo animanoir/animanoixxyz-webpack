c68499d5807ce2799f377eabef3f5ee7986029ab
Óscar A. Montiel <etherealflux94@gmail.com>
2021-10-29 18:33:12 -0500

    Adds better about me

--- src/script.js
+++ src/script.js
@@ -390,17 +390,17 @@ window.addEventListener('mousemove', (event) => {
 
 /* ------------------------------ Last.FM Data ------------------------------ */
 
-// const lastfmData = fetch('https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=swoephowx&api_key=8d1394415d95c0771ac9f8247cc7ee17&limit=1&nowplaying=true&format=json')
-//   .then(
-//     response => response.json()
-//   )
-//   .then(data => {
-//     // Removes quotes from JSON data
-//     const formattedTrackname = JSON.stringify(data.recenttracks.track[0].name).replace(/["]+/g, '')
-//     const formattedArtistname = JSON.stringify(data.recenttracks.track[0].artist['#text']).replace(/["]+/g, '')
-//     document.getElementById("track").textContent = formattedTrackname
-//     document.getElementById("artist").textContent = formattedArtistname
-//   });
+const lastfmData = fetch('https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=swoephowx&api_key=8d1394415d95c0771ac9f8247cc7ee17&limit=1&nowplaying=true&format=json')
+  .then(
+    response => response.json()
+  )
+  .then(data => {
+    // Removes quotes from JSON data
+    const formattedTrackname = JSON.stringify(data.recenttracks.track[0].name).replace(/["]+/g, '')
+    const formattedArtistname = JSON.stringify(data.recenttracks.track[0].artist['#text']).replace(/["]+/g, '')
+    document.getElementById("track").textContent = formattedTrackname
+    document.getElementById("artist").textContent = formattedArtistname
+  });
 
 /* ---------------------------------- gsap ---------------------------------- */
 
@@ -461,12 +461,12 @@ about.addEventListener('click', () => {
   gsap.to(camera.position, {
     duration: 0.8,
     z: aboutClicked ? 11 : 6,
-    ease: 'sine.out'
+    ease: 'sine.inOut'
   })
   gsap.to(aboutText, {
-    duration: 0.4,
+    duration: 0.5,
     left: aboutClicked ? '5vw' : '-50vw',
-    ease: 'sine.out',
+    ease: 'sine.inOut',
     opacity: aboutClicked ? 1 : 0
   })
 })
